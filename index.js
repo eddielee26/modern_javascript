fa9734e2b55f4189aec739c2f0609b40d07c0063
eddie <eddielee001@gmail.com>
2019-09-26 13:11:00 -0500

    adds npm, new js library

---
+++ index.js
@@ -0,0 +1,8 @@
+import moment from 'moment';
+var math = require('mathjs');
+
+console.log(moment().format());
+console.log(math.pow([[-1, 2], [3, 1]], 2));
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
